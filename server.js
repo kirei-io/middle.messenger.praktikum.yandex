aede0a4402d25c555ee3130c992706f9b4965486
kirei-io <mail.sweetlight@gmail.com>
2023-02-23 03:02:53 +0300

    fix: switch from ES modules to commonjs

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
 
 const app = express();
-
 const port = process.env.PORT ?? 3000;
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const dist = path.join(__dirname, "dist");
 
 app.use(express.static(dist));
