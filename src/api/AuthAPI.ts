681a475fe7ccbc69cfa2181563333e1847d07252
kirei-io <mail.sweetlight@gmail.com>
2023-03-31 14:15:15 +0300

    add auth api and controller

---
+++ src/api/AuthAPI.ts
@@ -0,0 +1,28 @@
+import { HTTPTransport, HTTPTransportConfig } from "../core/HTTPTransport";
+import {
+  SignInRequest,
+  SignInResponse,
+  SignUpRequest,
+  SignUpResponse,
+  UserResponse,
+} from "./AuthAPI.interface";
+import { BaseAPI } from "./BaseAPI";
+
+const AuthAPIHTTPinstanceConfig: HTTPTransportConfig = {
+  endpoint: "/auth",
+};
+const AuthAPIHTTPinstance = new HTTPTransport(AuthAPIHTTPinstanceConfig);
+
+class AuthAPI extends BaseAPI {
+  signin = (data: SignInRequest): Promise<SignInResponse> =>
+    this.apiInstance.post("/signin", { data });
+
+  signup = (data: SignUpRequest): Promise<SignUpResponse> =>
+    this.apiInstance.post("/signup", { data });
+
+  read = (): Promise<UserResponse> => this.apiInstance.get("/user");
+
+  logout = (): Promise<void> => this.apiInstance.post("/logout");
+}
+
+export default new AuthAPI(AuthAPIHTTPinstance);
